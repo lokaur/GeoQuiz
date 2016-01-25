8b32c398bcf448f3ef022f646601fc2cfdee98e6
Andrey Martynov <andremartynov@gmail.com>
2016-01-25 23:21:25 +0300

    challenge 3

--- app/src/main/java/net/ddns/andremartynov/geoquiz/QuizActivity.java
+++ app/src/main/java/net/ddns/andremartynov/geoquiz/QuizActivity.java
@@ -12,16 +12,14 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class QuizActivity extends AppCompatActivity {
 
 	public static final String TAG = "QuizActivity";
 
 	private Button mTrueButton;
 	private Button mFalseButton;
-	private Button mNextButton;
-	private Button mPrevButton;
+	private ImageButton mNextButton;
+	private ImageButton mPrevButton;
 	private TextView mQuestionTextView;
 	private int mCurrentIndex = 0;
 
@@ -71,7 +69,7 @@ public class QuizActivity extends AppCompatActivity {
 			}
 		});
 
-		mNextButton = (Button)findViewById(R.id.next_button);
+		mNextButton = (ImageButton)findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -80,7 +78,7 @@ public class QuizActivity extends AppCompatActivity {
 			}
 		});
 
-		mPrevButton = (Button)findViewById(R.id.prev_button);
+		mPrevButton = (ImageButton)findViewById(R.id.prev_button);
 		mPrevButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
